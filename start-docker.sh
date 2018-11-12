bc10129b52c4f10bc0a0626161b6fec914cffb56
Alan_Liu (RD) <alan@breaktime.com.tw>
2018-11-12 14:42:50 +0800

    Remove setting from configure and rename from kobuta to article.
    	deleted:    docs/api.html
    	deleted:    docs/system-vulnerability_scan_api_1.0.0_swagger.yaml
    	modified:   start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -22,7 +22,8 @@ docker run --net=dev-net --name article-dev -d -v $curDir/break-article/breakart
 docker exec -it article-dev python3 /opt/breaktime/initdb.py
 
 # Start nginx and forword request to break-article uwsgi:8700
-docker run --net=dev-net --name nginx -d -p 8080:80 nginx:1.10.3-alpine
+docker run --net=dev-net --name nginx -d -p 80:80 nginx:1.10.3-alpine
+# docker run --net=dev-net --name nginx -d -p 8080:80 nginx:1.10.3-alpine
 docker cp break-article/data/nginx/nginx.conf nginx:/etc/nginx
 docker cp break-article/data/nginx/conf.d/breaktime-article.conf nginx:/etc/nginx/conf.d/default.conf
 docker restart nginx
